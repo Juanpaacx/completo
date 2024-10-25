a9c5948dd78c6813a1ef890ceceb9ffd5ac7d3df
Your Name <juancoyotl170@gmail.com>
2024-10-24 20:07:48 -0600

    conexionn

--- Tienda/lib/view/electonica_view.dart
+++ Tienda/lib/view/electonica_view.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+/*import 'package:flutter/material.dart';
 
 import 'product_details_view.dart';
 
@@ -7,7 +7,8 @@ class ElectonicaView extends StatelessWidget {
     {
       'nombre': 'Camisa Casual',
       'precio': '25.00',
-      'imagen': 'https://via.placeholder.com/150'
+      'imagen':
+          'https://yazbek.com.mx/cdn/shop/products/C0306-playera-cv-mc-caballero-100algodon-blanco_2.jpg?v=1693280739'
     },
     {
       'nombre': 'Jeans Azul',
@@ -104,6 +105,143 @@ class ElectonicaView extends StatelessWidget {
                         style: const TextStyle(
                           fontWeight: FontWeight.bold,
                           fontSize: 14, // Tamaño de texto más pequeño
+                        ),
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: Text(
+                        '\$${producto['precio']}',
+                        style: const TextStyle(
+                          fontSize: 12,
+                          color: Colors.green,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+*/
+
+import 'package:flutter/material.dart';
+
+import 'product_details_view.dart';
+
+class ElectonicaView extends StatelessWidget {
+  final List<Map<String, String>> productos = [
+    {
+      'nombre': 'Camisa Casual',
+      'precio': '25.00',
+      'imagen':
+          'https://yazbek.com.mx/cdn/shop/products/C0306-playera-cv-mc-caballero-100algodon-blanco_2.jpg?v=1693280739'
+    },
+    {
+      'nombre': 'Jeans Azul',
+      'precio': '40.00',
+      'imagen': 'https://via.placeholder.com/150'
+    },
+    {
+      'nombre': 'Chaqueta de Cuero',
+      'precio': '120.00',
+      'imagen': 'https://via.placeholder.com/150'
+    },
+    {
+      'nombre': 'Zapatos Deportivos',
+      'precio': '60.00',
+      'imagen': 'https://via.placeholder.com/150'
+    },
+    {
+      'nombre': 'Camiseta Estampada',
+      'precio': '15.00',
+      'imagen': 'https://via.placeholder.com/150'
+    },
+    {
+      'nombre': 'Pantalón de Vestir',
+      'precio': '45.00',
+      'imagen': 'https://via.placeholder.com/150'
+    },
+    {
+      'nombre': 'Chamarra de Mezclilla',
+      'precio': '85.00',
+      'imagen': 'https://via.placeholder.com/150'
+    },
+    {
+      'nombre': 'Gorra Deportiva',
+      'precio': '20.00',
+      'imagen': 'https://via.placeholder.com/150'
+    },
+    {
+      'nombre': 'Sudadera Casual',
+      'precio': '50.00',
+      'imagen': 'https://via.placeholder.com/150'
+    },
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Productos Electrónicos'), // Corrige el título
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+            childAspectRatio: 3 / 4,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+          ),
+          itemCount: productos.length,
+          itemBuilder: (context, index) {
+            final producto = productos[index];
+            return GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => ProductDetailsView(
+                      nombre: producto['nombre']!,
+                      precio: producto['precio']!,
+                      imagen: producto['imagen']!,
+                    ),
+                  ),
+                );
+              },
+              child: Card(
+                elevation: 5.0,
+                shape: RoundedRectangleBorder(
+                  borderRadius:
+                      BorderRadius.circular(10.0), // Bordes redondeados
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: Image.network(
+                        producto['imagen']!,
+                        fit: BoxFit.cover,
+                        errorBuilder: (context, error, stackTrace) {
+                          return const Center(child: Icon(Icons.error));
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        producto['nombre']!,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 14,
                         ),
                         maxLines: 1,
                         overflow: TextOverflow.ellipsis,
